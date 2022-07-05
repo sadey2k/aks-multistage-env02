3c2ce0ea96d0dafc74f5b4eeed3c85f6354c64c2
Shola Ade <sadey2k@yahoo.com>
2022-07-05 19:04:51 +0100

    update1

--- tf-infra/main.tf
+++ tf-infra/main.tf
@@ -5,11 +5,8 @@ module "cluster" {
   aks_cluster_name                    = var.aks_cluster_name
   dns_prefix                          = var.dns_prefix
   agent_count                         = var.agent_count
-#   aks_service_principal_app_id        = var.aks_service_principal_app_id
-#   aks_service_principal_client_secret = var.aks_service_principal_client_secret
   location                            = var.location
   kubernetes_version                  = var.kubernetes_version
-  #ssh_public_key                      = var.ssh_public_key
   tags                                = var.tags
   access_key                          = var.access_key
 }
